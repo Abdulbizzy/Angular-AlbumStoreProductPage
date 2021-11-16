80067700292a22df48c4aec04c02a7ded77b1608
Abdulbizzy <abdulbasitsulaiman@yahoo.com>
2021-11-16 22:42:00 +0100

    hhh

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json'
 constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json())
-  }
+getAlbum(id: number): Observable<Album> {
+  return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+}
 }
